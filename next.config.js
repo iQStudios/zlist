3ba186c425db13c672ecb67c139c807913be7aaa
iQStudios <iqstudios.official@gmail.com>
2020-11-10 02:51:18 +0100

    new better

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_VALID_MAIL: "yanzailive@gmail.com",
-    NEXT_PUBLIC_DB_API_KEY: "AIzaSyDKeWyvBV6BCTFE9gnfX8IQICTTE-tiebo",
-    NEXT_PUBLIC_DB_AUTH_DOMAIN: "ristorantepechino-54081.firebaseapp.com",
-    NEXT_PUBLIC_DB_URL: "https://ristorantepechino-54081.firebaseio.com",
-    NEXT_PUBLIC_DB_PROJECT_ID: "ristorantepechino-54081",
-    NEXT_PUBLIC_DB_STORAGE_BUCKET: "ristorantepechino-54081.appspot.com",
-    NEXT_PUBLIC_DB_MESSA_SENDER_ID: "124500517860",
-    NEXT_PUBLIC_DB_APPID: "1:124500517860:web:1524877457f67c7b55ddf1",
-    NEXT_PUBLIC_DB_MEASUREMENT_ID: "G-G9RM72XCS0",
+    NEXT_PUBLIC_VALID_MAIL: "Your email login convalid",
+    NEXT_PUBLIC_DB_API_KEY: "firebase Api key",
+    NEXT_PUBLIC_DB_AUTH_DOMAIN: "firebase Auth Domain",
+    NEXT_PUBLIC_DB_URL: "firebase URL",
+    NEXT_PUBLIC_DB_PROJECT_ID: "firebase Project ID",
+    NEXT_PUBLIC_DB_STORAGE_BUCKET: "firebase storage bucket",
+    NEXT_PUBLIC_DB_MESSA_SENDER_ID: "firebase messa sender id",
+    NEXT_PUBLIC_DB_APPID: "firebase app id",
+    NEXT_PUBLIC_DB_MEASUREMENT_ID: "firebase  measurement id",
   },
   serverRuntimeConfig: {
     // Will only be available on the server side
